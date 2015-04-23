4b9d3a6546e1707ee5aaad5604346fb5bd1580b8
Phil Holden <philip.holden@ncr.com>
2015-04-23 09:12:13 +0100

    react style

---
+++ scripts/style/ClickCounterStyles.js
@@ -0,0 +1,16 @@
+import StyleSheet from 'react-style';
+
+export default StyleSheet.create`
+
+.button {
+  border: 0px solid black;
+  background: #6464FE;
+  color: white;
+  border-radius: 25px;
+}
+
+.value {
+  margin: 0 3px;
+}
+
+`;
